df873724d22e99b4054e491f598a49c9f4950748
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2023-05-20 17:20:34 -0400

    cache, remove special chars, misc tweaks

--- app.py
+++ app.py
@@ -1,68 +1,71 @@
 from langchain.vectorstores import Pinecone
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.callbacks import get_openai_callback
+from langchain.cache import SQLiteCache
+import langchain
+
 import pinecone
 import os
 import json
 import sys
-import urllib.parse
 
 model_name="sentence-transformers/all-mpnet-base-v2"
 model_kwargs = {}
 embeddings = HuggingFaceEmbeddings(model_name=model_name,cache_folder='/tmp',model_kwargs=model_kwargs)
 
-metadata_field_info = [
+metadata_field_info=[
     AttributeInfo(
         name="title",
-        description="The title of the movie",
-        type="string",
+        description="The title of the movie (in lowercase)", 
+        type="string", 
     ),
-    AttributeInfo(
-        name="description",
-        description="The description of the movie",
-        type="string",
-    ),
+    # AttributeInfo(
+    #     name="description",
+    #     description="The description of the movie (in lowercase)", 
+    #     type="string", 
+    # ),
     AttributeInfo(
         name="genre",
-        description="The genre(s) of the movie",
-        type="string or list[string]",
+        description="The genres of the movie (in lowercase)", 
+        type="string or list[string]", 
     ),
     AttributeInfo(
         name="certificate",
-        description="The certificate of the movie",
-        type="string",
+        description="The certificate of the movie", 
+        type="string", 
     ),
     AttributeInfo(
         name="year",
-        description="The year the movie was released",
-        type="integer",
+        description="The year the movie was released", 
+        type="integer", 
     ),
     AttributeInfo(
         name="stars",
-        description="The name of the movie actors",
-        type="string or list[string]",
+        description="The name of the movie actors (in lowercase)", 
+        type="string or list[string]", 
     ),
     AttributeInfo(
         name="directors",
-        description="The name of the movie directors",
-        type="string or list[string]",
+        description="The name of the movie directors (in lowercase)", 
+        type="string or list[string]", 
     ),
     AttributeInfo(
         name="runtime",
-        description="The runtime of the movie",
-        type="string",
+        description="The runtime of the movie in minutes", 
+        type="string", 
     ),
     AttributeInfo(
         name="imdb_rating",
         description="A 1-10 rating for the movie on IMDB",
-        type="float",
+        type="float"
     ),
-    AttributeInfo(
+     AttributeInfo(
         name="ratingCount",
-        description="How many people rated the movie on IMDB",
-        type="integer",
+        description="How many people rated the movie on IMDB. Indicator of movie's popularity",
+        type="integer"
     ),
 ]
 document_content_description = "Summary and plot of the movie"
@@ -75,28 +78,38 @@ def getResults(inputQuery):
     vectorstore = Pinecone.from_existing_index(
         os.environ["PINECONE_INDEX_NAME"], embeddings
     )
-    llm = OpenAI(temperature=0)
+    #Cache results in a local SQLite database
+    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+    langchain.llm_cache = SQLiteCache(database_path="/tmp/langchain.db")
+
     retriever = SelfQueryRetriever.from_llm(
         llm,
         vectorstore,
         document_content_description,
         metadata_field_info,
-        # enable_limit=True,
         verbose=True,
     )
-    # retriever.search_kwargs = {"k": 10}
-    results = retriever.get_relevant_documents(inputQuery)
-    titles = [result.metadata["source"]  for result in results]
-    return titles
+
+    with get_openai_callback() as cb:
+        retriever.search_kwargs = {"k": 20}
+        results = retriever.get_relevant_documents(inputQuery)
+        titles = [result.metadata["source"]  for result in results]
+        print(cb)
+        return titles
 
 def lambda_handler(event, context):
+    
     # Parse the incoming event data
     print('queryStringParameters:', json.dumps(event['queryStringParameters']))
     query = event['queryStringParameters']['query']
-    query =  urllib.parse.unquote(query)
+    
     #lowercase
     query = query.lower()
-    
+
+    #remove special characters
+    import re
+    query = re.sub(r'[^0-9A-Za-z -]', '', query)
+
     print('event:', json.dumps(event))
 
     response = {}
